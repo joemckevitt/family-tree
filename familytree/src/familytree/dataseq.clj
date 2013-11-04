cfbe6fd2d4249fa656c0b6f34e5352eebb8bf053
JoeMcKevitt <josephmckevitt@hotmail.com>
2013-11-04 21:55:18 +0000

    added another family (hardcoded - the smiths)

--- familytree/src/familytree/dataseq.clj
+++ familytree/src/familytree/dataseq.clj
@@ -14,22 +14,73 @@
             :role "mother"})
 
 (def Joe {:id 3
-          :firstname "Joe", :surname "McKevitt", :dob "9-3-1981" :role "son"})
+          :firstname "Joe",
+          :surname "McKevitt",
+          :dob "9-3-1981"
+          :role "son"})
 
-(def John {:id 4 :firstname "John", :surname "McKevitt", :dob "25-9-1982" :role "son"})
-(def James {:id 5 :firstname "James", :surname "McKevitt", :dob "4-1-1985" :role "son"})
-(def Marianne {:id 6 :firstname "Maz", :surname "McKevitt", :dob "27-8-1987" :role "daughter"})
-(def Claire {:id 7 :firstname "Claire", :surname "McKevitt", :dob "15-9-1988" :role "daughter"})
+(def John {:id 4
+           :firstname "John",
+           :surname "McKevitt",
+           :dob "25-9-1982"
+           :role "son"})
+
+(def James {:id 5
+            :firstname "James",
+            :surname "McKevitt",
+            :dob "4-1-1985"
+            :role "son"})
+
+(def Marianne {:id 6
+               :firstname "Maz",
+               :surname "McKevitt",
+               :dob "27-8-1987"
+               :role "daughter"})
+
+(def Claire {:id 7
+             :firstname "Claire",
+             :surname "McKevitt",
+             :dob "15-9-1988"
+             :role "daughter"})
+
+
+(def JohnSmith {:id 8
+             :firstname "John",
+             :surname "Smith",
+             :dob "15-9-1988"
+             :role "father"})
+
+(def MarySmith {:id 9
+                :firstname "Mary"
+                :surname "Smith"
+                :dob "14-12-1983"
+                :role "mother"})
+
+(def JimmySmith {:id 10
+                 :firstname "Jimmy"
+                 :surname "Smith"
+                 :dob "14-12-1999"
+                 :role "son"})
 
 (def mckevitts (list Noel, Marie, Joe, James, John, Marianne, Claire))
 (pprint mckevitts)
 
+(def smiths (list JohnSmith, MarySmith, JimmySmith))
+(pprint smiths)
+
+;data structure to contain a list of all the familes
+(def allfamiles (list smiths mckevitts))
+(pprint allfamiles)
+
 ;need to write a function which takes both a surname and firstname and returns back a set of matching
 ;families.  something like
 ;search-for-family(surname firstname(optional))
-(defn filter-on-firstname [firstname]
-  (filter #(= (:firstname %) firstname) mckevitts))
+(defn filter-on-firstname
+  [firstname]
+    (filter #(= (:firstname %) firstname) (flatten allfamiles))
+  )
 
 (filter-on-firstname "Joe")
+(filter-on-firstname "John")
 (filter-on-firstname "Noel")
 
